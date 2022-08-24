4cf924e3f73bf8ab7ad2cd875f2d753e87002b8a
Rahman Muhammad Mahbubur <43867380+pabelmahbub@users.noreply.github.com>
2022-08-24 23:29:14 +0900

    service get api done

--- index.js
+++ index.js
@@ -2,10 +2,36 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
+
+require('dotenv').config();
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ngyafc8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    await client.connect();
+    const serviceCollection = client.db("doctors_portal").collection("services");
+    console.log('HH');
+
+    app.get('/service', async (req,res)=>{
+      const query ={};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    })
+
+
+  }finally{
+
+  }
+}
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
   res.send('Hello');
